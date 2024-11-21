91c276d37c6d8237d8dc483ccfa9a7d2045ebf30
User <User@DESKTOP-UUIDEVT>
2024-11-22 00:04:03 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!\n");
+
+            string firstName = "Maaz";
+            string lastName = "Ahmed";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name+$"{"\n"}");
+        }
+    }
+}
+
